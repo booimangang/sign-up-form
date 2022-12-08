bc3e57b7b82d1bf6041e4aee87437e1dc3508b81
Booi <56810550+booimangang@users.noreply.github.com>
2022-12-08 23:52:28 +0530

    Add function to compare two password fields

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-
-// pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" 
-
+const form = document.getElementById('form');
 const firstNameInput = document.querySelector(`[name="first-name"]`);
 const passwordInput = document.querySelector(`#password`);
+const confirmPasswordInput = document.querySelector(`#confirm-password`);
 const showPasswordToggleBtn = document.querySelector(".show-password-toggle-tbn")
+const submitBtn = document.querySelector(".submit-btn")
+
+const firstNameErrorWarning = document.querySelector(".first-name-warning")
+const emailErrorWarning = document.querySelector(".email-warning")
+const passwordErrorWarning = document.querySelector(".invalid-password-warning")
+const unmatchPasswordWarning = document.querySelector(".unmatch-password-warning")
 
 firstNameInput.addEventListener("invalid", () => {
     if (firstNameInput.validity.tooShort) {
@@ -14,19 +19,34 @@ firstNameInput.addEventListener("invalid", () => {
 
 const error = document.querySelector("#mail + span.error")
 
+showPasswordToggleBtn.addEventListener("click", showPasswordToggleHandler)
 
-// passwordInput.addEventListener("input", () => {
-//     console.log("input password")
-// })
-
-showPasswordToggleBtn.addEventListener("click", (e) => {
+function showPasswordToggleHandler(e) {
     if (passwordInput.type === "password") {
-        passwordInput.type = "text"
-        showPasswordToggleBtn.textContent = "hide password"
+        passwordInput.type = "text";
+        confirmPasswordInput.type = "text";
+        showPasswordToggleBtn.textContent = "hide password";
     } else {
         passwordInput.type = "password";
-        showPasswordToggleBtn.textContent = "show password"
-
+        confirmPasswordInput.type = "password";
+        showPasswordToggleBtn.textContent = "show password";
     }
-})
+}
 
+form.addEventListener('submit', formSubmitHandler)
+
+function formSubmitHandler(e) {
+    let checkingPasswordFieldsCondition = checkPasswordFields(passwordInput.value, confirmPasswordInput.value)
+
+    if (!checkingPasswordFieldsCondition) {
+        e.preventDefault();
+        unmatchPasswordWarning.textContent = "passwords do not match"
+    }
+
+    console.log("submitting")
+}
+
+function checkPasswordFields(password, confirmedPassword) {
+    console.log(password == confirmedPassword)
+    return password == confirmedPassword
+}
